30f2284bc74ecf8336838a7b7e822ac6423eff73
SimonaPopescuIT <158314180+SimonaPopescuIT@users.noreply.github.com>
2024-06-20 21:05:58 +0300

    Simona Tours react-website-responsive

---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../App.css";
+
+import Navbar from "../Navbar";
+
+function SignUp() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="sign-up">LIKE & SUBSCRIBE</h1>;
+    </>
+  );
+}
+
+export default SignUp;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./components/pages/Home";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import SignUp from "./components/pages/SignUp";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<App />} />
+      <Route index element={<Home />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Route>
+  )
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
